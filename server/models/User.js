47182e40ed78b9576ebb00368d240faf5368e4aa
Abhishek0090 <palabhishek411@gmail.com>
2022-12-31 17:50:52 +0530

    added mvc structure

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,34 @@
 import mongoose from "mongoose";
 
 
-const UserSchema = () => {
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String
+    },
+    subscribers: {
+        type: Number,
+        default: 0
+    },
+    subscribedUsers: {
+        type: [String]
+    }
 
-    mongoose.schema({
+}, { timestamps: true })
 
-    })
 
-}
 
-export default UserSchema;
+export default mongoose.model("User",UserSchema); 
