fc8fcdf3d549658f106d1a41fa2f79a75843725d
Abhishek Pal <palabhishek411@gmail.com>
2023-01-10 22:12:04 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
-// import commentRoutes from "./routes/comments.js";
+import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auths.js";
 import cookieParser from "cookie-parser";
 
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
-// app.use("/api/comments", commentRoutes);
+app.use("/api/comments", commentRoutes);
 
 
 //error handler
