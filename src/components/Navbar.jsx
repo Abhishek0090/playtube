7bda5da3311c53c1af1e4110159059f0f3d2a8ee
Abhishek0090 <palabhishek411@gmail.com>
2023-02-23 16:59:19 +0530

    added tooltip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,26 @@ import { Link, useNavigate } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
 
 
+
+
 import youtube from "../img/youtube-logo-png-2067.png";
 import { DarkModeContext } from "../content/DarkModeContext";
 import { useSelector } from "react-redux";
 import Upload from "./Upload";
 
+ 
+import Box from '@mui/material/Box';
+import Avatar_icon from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 
 const Container = styled.div`
   position: sticky;
@@ -158,6 +173,15 @@ const Navbar = () => {
 
   const { darkMode, toggle, hamburger } = useContext(DarkModeContext);
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open_anchor = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <Container>
@@ -182,11 +206,85 @@ const Navbar = () => {
               <VideoCallOutlinedIcon onClick={() => setOpen(true)} />
               <Avatar src={currentUser?.img} alt={currentUser.img} />
               {currentUser.name}
-            
+        
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}> 
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open_anchor ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open_anchor ? 'true' : undefined}
+          >
+            <Avatar_icon sx={{ width: 32, height: 32 }} src={currentUser?.img} alt={currentUser.img}>{currentUser.name}</Avatar_icon>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open_anchor}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar_icon /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar_icon /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
  
-            <Logoutbutton>
-              Logout
-            </Logoutbutton>
             </User>
             
           </>
